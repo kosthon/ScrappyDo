78bc8f8f40b41204f243bb04673356f63149b22b
kosthon <nforero@codaltec.com>
2023-06-27 14:51:07 -0500

    🐍 Testeo de selenium para hacer web scrapping

--- index.py
+++ index.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import time
@@ -18,18 +19,30 @@ ChromeDriverManager().install()
 # Inicializar el navegador
 driver = webdriver.Chrome(options=options)
 
-driver.get('https://google.com')
+driver.get('https://www.windy.com/es/-Presi%C3%B3n-pressure?pressure,42.163,-64.863,3,m:eM0adx7')
 
 # Esperar a que el elemento sea clickable
 textarea = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, 'textarea.gLFyf'))
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'input#q'))
 )
 
 # Hacer clic en el elemento
 textarea.click()
 
+time.sleep(3)
+
 # Escribir en el elemento
-textarea.send_keys('CODALTEC')
+textarea.send_keys('Bogotá')
+textarea.send_keys(Keys.ENTER)
+time.sleep(2)
+textarea.send_keys(Keys.ENTER)
+time.sleep(2)
+textarea.send_keys(Keys.ENTER)
+time.sleep(3)
+
+ciudad = driver.find_element_by_xpath('//div[@data-label="Bogotá" and @class="city-1"]')
+
+ciudad.click()
 
 # Dar click en el boton buscar
 button = WebDriverWait(driver, 10).until(
@@ -38,5 +51,13 @@ button = WebDriverWait(driver, 10).until(
 
 button.click()
 
+# Dar click en el link de CODALTEC
+link = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'h3.LC20lb'))
+)
+
+link.click()
+
+
 # Pausa de 5 segundos para visualizar el resultado
-time.sleep(5)
+time.sleep(10)
