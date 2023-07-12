412a55bb5883f155822147daaf8adb853640e95e
kosthon <nforero@codaltec.com>
2023-07-12 13:54:25 -0500

    ⚙ Add capture grades of temperature

--- index.py
+++ index.py
@@ -5,13 +5,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import pandas as pd
+import pyautogui
 
 # Opciones de navegación
 options = Options()
 options.add_argument('--start-maximized')
 options.add_argument('--disable-extensions')
+options.add_argument('--incognito') 
 
 # Instalar automáticamente el ChromeDriver
 ChromeDriverManager().install()
@@ -22,8 +25,8 @@ driver = webdriver.Chrome(options=options)
 driver.get('https://www.windy.com/es/-Temperatura-temp?temp')
 
 # Digitar coordenadas
-latitud = input("Ingresa la latitud: ")
-longitud = input("Ingresa la longitud: ")
+    # latitud = input("Ingresa la latitud: ")
+    # longitud = input("Ingresa la longitud: ")
 
 # Esperar a que el elemento sea clickable
 textarea = WebDriverWait(driver, 10).until(
@@ -36,35 +39,43 @@ textarea.click()
 time.sleep(3)
 
 # Escribir en el elemento
-textarea.send_keys(latitud + ', ' + longitud)
-textarea.send_keys(Keys.ENTER)
-time.sleep(2)
-textarea.send_keys(Keys.ENTER)
-time.sleep(2)
+textarea.send_keys("6.1622, -73.8117")
 textarea.send_keys(Keys.ENTER)
 time.sleep(3)
-
-# Dar click en el boton buscar
-button = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, 'input.gNO89b'))
-)
-time.sleep(3)
-button.click()
-time.sleep(3)
-
-# Dar click en el boton de cerrar menu inferior
-closeButton = WebDriverWait(driver, 10).until(
-    EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.closing-x'))
-)
-time.sleep(3)
-closeButton.click()
-time.sleep(3)
+textarea.send_keys(Keys.ENTER)
+time.sleep(5)
 
 # Dar click en el dot del punto en el mapa
-    # time.sleep(3)
-    # dotLocation = WebDriverWait(driver, 10).until(
-    #     EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.leaflet-marker-icon.icon-dot'))
-    # )
+# Realizar la acción de clic derecho utilizando pyautogui
+dotElement = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'div.leaflet-marker-icon.icon-dot'))
+)
+
+def perform_right_click(element):
+    # Obtener la posición del elemento en la ventana del navegador
+    location = element.location
+    x = location['x']
+    y = location['y'] + 120
+
+    # Simular el clic derecho utilizando pyautogui
+    pyautogui.moveTo(x, y)
+    pyautogui.click(button='right')
+    
+
+perform_right_click(dotElement)
+time.sleep(3)
+
+temperatureOption = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[data-do="picker"]'))
+)
+
+temperatureOption.click()
+time.sleep(3)
+
+# Tomar los grados del picker
+pickerGrades = driver.find_element(By.CSS_SELECTOR, 'div.picker-content span[data-ref="content"] big[data-do="changeMetric"]')
+numberGrades = pickerGrades.text.split()[0]
+print(numberGrades)
 
 # Pausa de 5 segundos para visualizar el resultado
 time.sleep(10)
