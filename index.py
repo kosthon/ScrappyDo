4753bda31f4a45185466577e057c46f56b2c88ae
ST-IMBACUAN <129553241+ST-IMBACUAN@users.noreply.github.com>
2023-07-21 10:21:28 -0500

    geojson index.py

--- index.py
+++ index.py
@@ -5,10 +5,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-import time
 import csv
 import json
-from datetime import datetime
+
 
 # Opciones de navegación
 options = Options()
@@ -30,13 +29,18 @@ temperatura_element = WebDriverWait(driver, 30).until(
     EC.visibility_of_element_located((By.CSS_SELECTOR, 'big[data-do="changeMetric"]'))
 )
 temperatura = temperatura_element.text
-
 humedad = "70%"
 presion = "25°C"
+latitud = 42.1234
+longitud = -71.5678
+
 # Guardar información en un archivo
 
 # Ruta del archivo
 ruta_archivo = "datos.csv"
+# Ruta del archivo JSON
+ruta_archivojson = "datos.json"
+
 
 # Abrir el archivo en modo escritura, utilizando el modo "append"
 with open(ruta_archivo, "a", newline="") as archivo_csv:
@@ -54,14 +58,16 @@ datos = {
     "presion": presion
 }
 
-# Ruta del archivo JSON
-ruta_archivojson = "datos.json"
-
 try:
     with open(ruta_archivojson, "r") as archivo_json:
-        consultas_previas = json.load(archivo_json)
+        contenido_json = archivo_json.read()
+        if contenido_json:
+            consultas_previas = json.loads(contenido_json)
+        else:
+            consultas_previas = {}
 except FileNotFoundError:
     consultas_previas = {}
+
 
 # Generar un identificador único para la nueva consulta
 identificador = len(consultas_previas) + 1
@@ -80,6 +86,27 @@ with open(ruta_archivojson, "w") as archivo_json:
     json.dump(consultas_previas, archivo_json)
 
 print("Datos guardados en el archivo JSON:", ruta_archivojson)
+
+
+# Crear objeto GeoJSON
+geojson = {
+    "type": "Feature",
+    "geometry": {
+        "type": "Point",
+        "coordinates": [longitud, latitud]
+    },
+    "properties": {
+        "temperatura": temperatura,
+        "humedad": humedad,
+        "presion": presion
+    }
+}
+
+# Guardar información en archivo GeoJSON
+ruta_geojson = 'datos.geojson'
+with open(ruta_geojson, 'w') as archivo_geojson:
+    json.dump(geojson, archivo_geojson)
+print("Datos guardados en el archivo GeoJSON:", ruta_geojson)
 
 # Cerrar el navegador
 driver.quit()
